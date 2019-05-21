cc3c026c245872e4f311d13903636ed55ec7bfcb
onnion <marcio.hsdl@gmail.com>
2019-05-21 17:27:58 -0300

    [master] | links

--- src/app/modules/shared/sidebar/sidebar.component.ts
+++ src/app/modules/shared/sidebar/sidebar.component.ts
@@ -4,6 +4,9 @@ import { RouteInfo, ROUTES } from './sidebar.config';
 import { User } from '../../../models/user.model';
 import { AuthService } from '../../../services/auth/auth.service';
 import { AclService } from 'ng2-acl';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ResponseContentType } from '@angular/http';
 
 @Component({
     selector: 'app-sidebar-cmp',
@@ -13,7 +16,7 @@ export class SidebarComponent implements OnInit {
     public menuItems: RouteInfo[];
     public user: User;
 
-    constructor(private authService: AuthService, private aclService: AclService) {
+    constructor(private http: HttpClient, private authService: AuthService, private aclService: AclService) {
     }
 
     public hasPermission(permission: any): boolean {
@@ -32,7 +35,7 @@ export class SidebarComponent implements OnInit {
     public updatePS(): void {
         if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
             const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
-            let ps = new PerfectScrollbar(elemSidebar, {wheelSpeed: 2, suppressScrollX: true});
+            let ps = new PerfectScrollbar(elemSidebar, { wheelSpeed: 2, suppressScrollX: true });
         }
     }
 
@@ -43,4 +46,26 @@ export class SidebarComponent implements OnInit {
     public logout(): void {
         this.authService.logout();
     }
+
+    // public getReceipt(url: string) {
+    //     console.log(url);
+    //     return this.http
+    //         .get(url, {responseType: 'blob'})
+    //         .subscribe((res: any) => {
+    //             console.log('start download:', res);
+    //             var url = window.URL.createObjectURL(res.data);
+    //             var a = document.createElement('a');
+    //             document.body.appendChild(a);
+    //             a.setAttribute('style', 'display: none');
+    //             a.href = url;
+    //             a.download = res.filename;
+    //             a.click();
+    //             window.URL.revokeObjectURL(url);
+    //             a.remove(); // remove the element
+    //         }, error => {
+    //             console.log('download error:', JSON.stringify(error));
+    //         }, () => {
+    //             console.log('Completed file download.')
+    //         });
+    // }
 }
