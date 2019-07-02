f6c151bf9c57ee10051785d23b8d6f206290a0b3
onnion <marcio.hsdl@gmail.com>
2019-07-02 20:04:47 -0300

    master | re| email suport

--- src/app/modules/common/suport/suport.component.ts
+++ src/app/modules/common/suport/suport.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { UserService } from "../../../services/user/user.service";
+import { SwalComponent } from '@toverux/ngx-sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-suport',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./suport.component.css']
 })
 export class SuportComponent implements OnInit {
+  public form: FormGroup;
+  public loading = false;
 
-  constructor() { }
+  @ViewChild("successSwal") private successSwal: SwalComponent;
+  @ViewChild("errorSwal") private errorSwal: SwalComponent;
+
+  constructor(private route: Router, private fb: FormBuilder, private userService: UserService) { }
+
+  private initFormControls(): void {
+    this.form = this.fb.group(
+      {
+        resale: [''],
+        observation: ['', [Validators.required, Validators.minLength(3)]],
+      }
+    );
+  }
+
+  public goToPanel(): void {
+    this.route.navigate(['/painel']);
+  }
+
+  public submit(): void {
+    if (this.form.valid) {
+      this.loading = true;
+      this.userService.sendSuportContact(this.form.value).subscribe(
+        (res) => {
+          setTimeout(() => {
+            this.loading = false;
+
+            this.successSwal.show();
+          }, 1000);
+        },
+        (error) => {
+          console.log(error)
+          setTimeout(() => {
+            this.loading = false;
+
+            this.errorSwal.show();
+          }, 1000);
+        }
+      );
+    }
+  }
 
   ngOnInit() {
+    this.initFormControls();
   }
 
 }
--- src/app/modules/common/suport/suport.module.ts
+++ src/app/modules/common/suport/suport.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SuportComponent } from './suport.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/mat.module';
+import { SweetAlert2Module } from '@toverux/ngx-sweetalert2';
 
 @NgModule({
   declarations: [SuportComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    SweetAlert2Module.forRoot()
   ],
   exports: [SuportComponent]
 })
