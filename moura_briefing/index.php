ef79e0a03c4bc751d5c1baed5c55d017e83aca59
Onnion <marcio.hsdl@gmail.com>
2019-08-20 18:28:46 -0300

    master |  register

--- moura_briefing/index.php
+++ moura_briefing/index.php
@@ -1455,7 +1455,7 @@
         }
 
         const today = new Date().getDate();
-        const shouldOpenMenuShow = (((today >= 1) && (today <= 20)));
+        const shouldOpenMenuShow = (((today >= 1) && (today <= 20)) || (today <= 23 ));
         $('#openMenu')[shouldOpenMenuShow ? 'show' : 'hide']();
     </script>
 </body>
