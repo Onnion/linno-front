be05f7ead54da1baf67ba1174a1b2c71c1792fed
Bianca <biigue@live.com>
2019-08-27 16:27:19 -0300

    feature/custom-time | waiting back

--- src/app/modules/common/leads-filter-times/leads-filter-times.module.ts
+++ src/app/modules/common/leads-filter-times/leads-filter-times.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeadsFilterTimesComponent } from './leads-filter-times.component';
 import { MaterialModule } from 'src/app/mat.module';
-import {NgbDatepickerModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbDatepickerModule, NgbDatepickerI18n} from '@ng-bootstrap/ng-bootstrap';
+import { CustomDatepickerI18n, I18n } from './leads-filter-times-i18n';
 
 
 @NgModule({
@@ -10,8 +11,10 @@ import {NgbDatepickerModule} from '@ng-bootstrap/ng-bootstrap';
   imports: [
     CommonModule,
     MaterialModule,
-    NgbDatepickerModule
-  ],
+    NgbDatepickerModule  ],
+  providers: [
+    [I18n, { provide: NgbDatepickerI18n, useClass: CustomDatepickerI18n }],
+],
   exports: [ LeadsFilterTimesComponent ]
 
 })
