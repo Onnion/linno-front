42d273b98078b647fcb12c6bbce7c1cdd25801cf
onnion <marcio.hsdl@gmail.com>
2019-09-01 16:19:04 -0300

    production | test

---
+++ dist/modules-admin-admin-module-es5.js
@@ -0,0 +1,48 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["modules-admin-admin-module"],{
+
+/***/ "./src/app/modules/admin/admin.module.ts":
+/*!***********************************************!*\
+  !*** ./src/app/modules/admin/admin.module.ts ***!
+  \***********************************************/
+/*! exports provided: AdminModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AdminModule", function() { return AdminModule; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/__ivy_ngcc__/fesm5/core.js");
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/__ivy_ngcc__/fesm5/http.js");
+
+// import { AppRoutingModule } from './app-routing.module';
+
+
+var AdminModule = /** @class */ (function () {
+    function AdminModule() {
+    }
+    AdminModule.ngModuleDef = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineNgModule"]({ type: AdminModule });
+    AdminModule.ngInjectorDef = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineInjector"]({ factory: function AdminModule_Factory(t) { return new (t || AdminModule)(); }, imports: [[
+                // AppRoutingModule,
+                _angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpClientModule"]
+            ]] });
+    return AdminModule;
+}());
+
+/*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵsetNgModuleScope"](AdminModule, { imports: [
+        // AppRoutingModule,
+        _angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpClientModule"]] });
+/*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵsetClassMetadata"](AdminModule, [{
+        type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"],
+        args: [{
+                declarations: [],
+                imports: [
+                    // AppRoutingModule,
+                    _angular_common_http__WEBPACK_IMPORTED_MODULE_1__["HttpClientModule"]
+                ],
+            }]
+    }], null, null);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=modules-admin-admin-module-es5.js.map
