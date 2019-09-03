66b0131864b386a0ad8e7bd6a07e6ba140499d3b
Onnion <marcio.hsdl@gmail.com>
2019-09-03 12:55:29 -0300

    master | router transition

--- src/app/modules/app/views/app/app.component.ts
+++ src/app/modules/app/views/app/app.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category/category.service';
 import { ProductService } from '../../services/product/product.service';
+import { routerTransition } from '../../animations/animations';
 
 @Component({
   selector: 'app-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [routerTransition]
 })
 export class AppComponent implements OnInit {
 
   constructor(private productService: ProductService, private categoryService: CategoryService) { }
-
-  private checkMobile() {
-    
-  }
 
   ngOnInit() {
     this.categoryService.get();
