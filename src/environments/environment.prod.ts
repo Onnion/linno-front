815fab75087b070daa6c275ee3d1eb33e9df1865
root <marcio.hsdl@gmail.com>
2019-11-04 15:34:38 -0300

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  AUTH_URL: 'http://apilinno.impactadigital.in',
+  AUTH_URL: 'https://apilinno.impactadigital.in',
   GRANT_TYPE: 'password',
   CLIENT_SECRET: 'dHcUGA83DshjbUtwZPgNUQwVAH3Z0cKEpDyhFtBb',
   CLIENT_ID: 2
