04e272fa851575e230f92268523a62b7e753790c
Onnion <marcio.hsdl@gmail.com>
2019-12-06 02:36:56 -0300

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   AUTH_URL: 'https://apilinno.impactadigital.in',
   GRANT_TYPE: 'password',
-  CLIENT_SECRET: 'dHcUGA83DshjbUtwZPgNUQwVAH3Z0cKEpDyhFtBb',
+  CLIENT_SECRET: 'OzFWW3UXcxu9cGxxE6smzILpFVhDOaxOJ1EjiCQp',
   CLIENT_ID: 2
 };
